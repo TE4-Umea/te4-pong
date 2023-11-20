2db755d6b3a7eb9194132ad35972bbf75eee8a8d
Timpan42 <tim@fagerdal.se>
2023-11-20 13:53:51 +0100

    koplat knabbar med item script och ändrat namn samt flyttat item i filsystem

--- Scenes/UI_Item.gd
+++ Scenes/UI_Item.gd
@@ -1,25 +1,31 @@
 extends Control
+var labol_color = "blue"
+var pros_color = "green"
+var cons_color = "red"
+
+signal yes_button_pressed
+signal no_button_pressed
 
 
 func _ready():
-	set_label_name("awdawdawdawd", "blue")
-	set_decsription("hej ¥luck€ ¤death¡ ¥ajnwdkjn€ ¤awdawd¡")
+	set_label_name("awdawdawdawd")
+	# För pros(grön) använd = ¥text€
+	# För cons(röd) använd = ¤textæ
+	set_decsription("hej ¥luck€ ¤deathæ ¥ajnwdkjn€ ¤awdawdæ")
 
-func set_label_name(name, color):
+func set_label_name(name):
 	var label_name = $BackPanel/ItemName
-	#Namn kan inte vara över 34 tecken, anars försviner den 
+	#Namn kan inte vara över 34 tecken, anars försviner texten 
 	label_name.append_text("[font_size={20}][color=" + 
-	color + "][center][b]" +
+	labol_color + "][center][b]" +
 	name + "[/b][/center][/color][/font_size]")
 
 func set_decsription(text):
 	text = "[font_size={15}] " + text + " [/font_size]"
 	var decript = $BackPanel/Decription
-	var pros_color = "green"
-	var cons_color = "red"
 	var text_lengt = len(text)
 	var count_pros = text.count("¥", 0, text_lengt )
-	var count_cons = text.count("€", 0, text_lengt )	
+	var count_cons = text.count("€", 0, text_lengt )
 	var word_index1 = 0
 	var word_index2 = 0
 	
@@ -35,7 +41,7 @@ func set_decsription(text):
 	
 	for n in range(count_cons):
 		word_index1 = text.find("¤", word_index1) + 1
-		word_index2 = text.find("¡", word_index1)
+		word_index2 = text.find("æ", word_index1)
 		var test = text.substr(word_index1, word_index2 - word_index1)
 		test = test.to_upper()
 		text = text.replacen(test, test)
@@ -43,10 +49,12 @@ func set_decsription(text):
 	text = text.replace("¥", "[color=" + pros_color + "]" )
 	text = text.replace("€", "[/color]")
 	text = text.replace("¤", "[color=" + cons_color + "]")
-	text = text.replace("¡", "[/color]")	
+	text = text.replace("æ", "[/color]")
 	
-	print(text)
 	decript.append_text(text)
-	
-	
 
+func _on_yes_button_pressed():
+	yes_button_pressed.emit()
+
+func _on_no_button_pressed():
+	no_button_pressed.emit()
