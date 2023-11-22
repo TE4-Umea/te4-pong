cabbcef3ec4196ce916f8ec90e59b626ad90fe70
RasmusJohansson04 <ras.johansson04@gmail.com>
2023-11-22 11:25:05 +0100

    Draws full overworld

--- Scenes/Map/OverWorld/MapNode.gd
+++ Scenes/Map/OverWorld/MapNode.gd
@@ -8,13 +8,21 @@ func _ready():
 	pass
 
 func _draw():
-	$Label.text = str(column)
+	draw_circle(Vector2.ZERO, 3, Color.BLACK)
+	$Label.text = str(id)
 	var nodes = get_tree().get_nodes_in_group("map_node")
-	for node in nodes:
-		if node.column == (column + 1):
-			if node.id < id +2 and node.id > id -2: 
-				print(str(node.column) + "==" + str(column+1))
-				draw_line(Vector2.ZERO, node.position - position, Color.BLACK)
+	if column % 2 == 0:
+		for node in nodes:
+			if node.column == (column + 1):
+				if node.id == id or id + 1 == node.id: 
+					print(str(node.column) + "==" + str(column+1))
+					draw_line(Vector2.ZERO, node.position - position, Color.BLACK)
+	else:
+		for node in nodes:
+			if node.column == (column + 1):
+				if node.id == id or id - 1 == node.id: 
+					print(str(node.column) + "==" + str(column+1))
+					draw_line(Vector2.ZERO, node.position - position, Color.BLACK)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- Scenes/Map/OverWorld/Map_Init.gd
+++ Scenes/Map/OverWorld/Map_Init.gd
@@ -10,7 +10,7 @@ var positions_to_draw : Array[Vector2] = []
 
 func _ready():
 	point_position = Vector2(64, get_viewport_rect().size.y / 2)
-	_add_point(point_position, -1, 0)
+	_add_point(point_position, -1, 1)
 	for index in range(length):
 		point_position = Vector2(point_position.x + x_gap, get_viewport_rect().size.y / 2)
 		var points_in_column = 3
@@ -21,7 +21,7 @@ func _ready():
 		for point in range(points_in_column):
 			_add_point(point_position, index, point)
 	point_position = Vector2(point_position.x + x_gap, get_viewport_rect().size.y / 2)
-	_add_point(point_position, length, 0)
+	_add_point(point_position, length, 1)
 
 func _add_point(position_for_point : Vector2, column : int, id : int):
 	point_position.y += y_gap
@@ -31,10 +31,6 @@ func _add_point(position_for_point : Vector2, column : int, id : int):
 	m.column = column
 	m.id = id
 	positions_to_draw.append(m.position)
-
-func _draw():
-	for pos in positions_to_draw:
-		draw_rect(Rect2(pos.x-2, pos.y-2, 4, 4), Color.BLACK)
 
 func _process(delta):
 	pass
