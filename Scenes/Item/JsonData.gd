3c1d440ca087234f2331b5a4d9cef509677a40ba
Timpan42 <tim@fagerdal.se>
2023-11-22 14:08:17 +0100

    kan hämnta data från json fil och alla funktioner som behövs

--- Scenes/Item/JsonData.gd
+++ Scenes/Item/JsonData.gd
@@ -1,19 +1,40 @@
 extends Node
 
-var item_data = {}
-var data_file_path = "res://Scenes/Items/destruktiv.json"
+var data_file_path = "res://Scenes/Item/destruktiv.json"
 
-func _ready():
-	item_data = load_json_file(data_file_path)
-
-func load_json_file(filPath : String):
+func load_json_file(filPath : String, id):
 	if FileAccess.file_exists(filPath):
 		var dataFile = FileAccess.open(filPath, FileAccess.READ)
 		var parseResult = JSON.parse_string(dataFile.get_as_text())
-		
-		if parseResult is Dictionary:
-			return parseResult
+		if parseResult is Array:
+			return parseResult[id]
 		else:
 			print("Error reading file!")
 	else:
 		print("file no exist!")
+
+func get_json_spesific(object, id):
+	var item_data = load_json_file(data_file_path, id)
+	return item_data[object] 
+
+func get_json_stats(object, id):
+	var item_data = load_json_file(data_file_path, id)
+	return item_data["stats"][object]
+
+func get_json_name(id):
+	return get_json_spesific("name", id)
+
+func get_json_desc(id):
+	return get_json_spesific("desc", id)
+
+func get_json_ability(id):
+	return get_json_spesific("ability", id)
+
+func get_json_rarity(id):
+	return get_json_spesific("rarity", id)
+
+func get_json_element(id):
+	return get_json_spesific("element", id)
+
+func get_json_damage(id):
+	return get_json_stats("damage", id)
