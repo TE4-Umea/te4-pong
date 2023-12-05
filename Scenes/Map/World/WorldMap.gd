6e85730a82a5b12bcef331019ad22b85717fe868
RasmusJohansson04 <ras.johansson04@gmail.com>
2023-12-05 11:17:56 +0100

    World map works now

--- Scenes/Map/World/WorldMap.gd
+++ Scenes/Map/World/WorldMap.gd
@@ -1,7 +1,8 @@
 extends Node
 
 @export var tile_scene : PackedScene
-@export var encounters = 7
+@export var enemy_risk = 25
+@export var enemies = 8
 
 var id
 var map = []
@@ -11,10 +12,13 @@ var unused_tiles = []
 var enemy_tiles = []
 var offset : int
 var end
+var key
+var has_key = false
 
 func _ready():
 	map = MapManager.current_map
 	id = MapManager.current_world
+	var key_location = randi_range(1, map.size()-2)
 	for location in map:
 		var tile : Area2D = tile_scene.instantiate()
 		add_child(tile)
@@ -32,24 +36,28 @@ func _ready():
 		elif map.find(location) == map.size()-1:
 			tile.modulate = Color.RED
 			end = location
+		elif map.find(location) == key_location:
+			key = location
+			tile.modulate = Color.YELLOW
 		else:
-			unused_tiles.append(tile)
+			unused_tiles.append(location)
 			tile.modulate = Color.DIM_GRAY
-	var random_tile = unused_tiles.pick_random()
-	unused_tiles.erase(random_tile)
-	random_tile.modulate = Color.YELLOW
-	for encounter in encounters:
-		random_tile = unused_tiles.pick_random()
-		unused_tiles.erase(random_tile)
-		random_tile.modulate = Color.ORANGE
-		enemy_tiles.append(random_tile)
 
 func _move(direction):
 	if player + direction in map:
 		player += direction
 		$Player.position += Vector2(direction.x * offset, direction.y * offset)
-		if player == end:
-			get_tree().change_scene_to_file("res://Scenes/Map/Overworld/Overworld.tscn")
+		if player == key and not has_key:
+			print("Picked up Key")
+			has_key = true
+		elif player == end:
+			if has_key:
+				get_tree().change_scene_to_file("res://Scenes/Map/Overworld/Overworld.tscn")
+		else:
+			var random = randi_range(0, 100)
+			if random < enemy_risk and enemies > 0:
+				enemies -= 1
+				print("ENEMY")
 
 func _process(delta):
 	if Input.is_action_just_pressed("P1_UP"):
