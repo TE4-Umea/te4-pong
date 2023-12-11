cdfd1f2b1ac8947f40071c975978e737b26eb964
Timpan42 <tim@fagerdal.se>
2023-12-11 09:51:17 +0100

    nu hämntas bild till item knapperna och när man selectar ett item i UI_item

--- Scenes/Item/item_base.gd
+++ Scenes/Item/item_base.gd
@@ -4,7 +4,12 @@ var mouse_focus = False.FalseFalse
 signal button_pressed
 var item_index
 var item_stats : Array
+var item_url = "res://Assets/Img/Item Icons/"
+var item_name
+var dot_png = ".png"
 
+func _ready():
+	print("get image")
 
 func _on_button_focus_entered():
 	mouse_focus = True.TrueTrue
@@ -17,4 +22,6 @@ func set_item_index(number):
 	item_index = number
 
 func set_item_stats(item):
+	item_name = item[0]
+	$Button/TextureRect.texture = load(item_url + item_name + dot_png)
 	item_stats.append_array(item)
